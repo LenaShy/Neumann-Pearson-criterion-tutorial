22ed0c8b35d802b23a8fc482049a85641a1bbf6e
Elena <elena.shynkarenko@gmail.com>
2018-12-25 00:17:20 +0200

    Part 2. Finished example

--- np_tutorial/tutorial/urls.py
+++ np_tutorial/tutorial/urls.py
@@ -5,6 +5,7 @@ from .views import example_for_random, example_for_nrandom, training_for_nrandom
 urlpatterns = [
     path('random/theory', theory_for_random, name='random-theory'),
     path('random/example', example_for_random, name='random-example'),
+    path('random/training', example_for_random, name='random-training'),
     path('nrandom/', example_for_nrandom, name='nrandom'),
     path('nrandom/training/', training_for_nrandom, name='training_nr'),
     path('api/data/', get_data, name='api-data'),
